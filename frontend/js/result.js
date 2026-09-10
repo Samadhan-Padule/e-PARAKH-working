e21b242f07d7d34f3f944f6676e877ace5dae69b
Shambho-studio <samdhanpadule1@gmail.com>
2026-09-10 22:10:04 +0530

    fix result backend https url

--- frontend/js/result.js
+++ frontend/js/result.js
@@ -2504,7 +2504,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     ===================================================== */
 
     const API_BASE_URL =
-        "http://e-parakh.onrender.com/api";
+        "https://e-parakh.onrender.com/api";
 
 
     let inspectionSavePromise =
